65afad57e80944970e29129259952dfc939618ea
Tauckl <155366460+Tauckl@users.noreply.github.com>
2024-01-01 13:55:13 +0100

    Add files via upload

---
+++ TaucklsStuffPack/Plugin.cs
@@ -0,0 +1,18 @@
+﻿using BepInEx;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaucklsStuffPack
+{
+    [BepInPlugin("com.tauckl.lcstuffpack", "TaucklsStuffPack", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+
+        }
+    }
+}
